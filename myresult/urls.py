2aaca3271b1a718f94d93fe5afe961e60bebc862
Rishi Ranjan <35308965+rishiRanjan111@users.noreply.github.com>
2020-10-01 08:31:49 +0530

    include_explanation

--- myresult/urls.py
+++ myresult/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+#here include is used for ...they include the urls created in apps
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
